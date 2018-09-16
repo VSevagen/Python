e36d3875d4d8d81e1fde04c502ad2eed52029c00
VSevagen <43094266+VSevagen@users.noreply.github.com>
2018-09-16 03:04:39 -0700

    Add files via upload
    Using beautiful soup 4 to search on google and return the top results

---
+++ scaping.py
@@ -0,0 +1,16 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+search_item = 'foss@amrita'
+url = 'https://www.google.co.in/search?q=' + search_item
+r = requests.get(url)
+html_content = r.text
+soup = BeautifulSoup(html_content, 'lxml')
+
+x = 0
+d = soup.find_all('a')
+for title in d:
+    x = x + 1
+    print (title.get_text())
+    
