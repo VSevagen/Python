f017227c069d1433edc48de745d4df50faebb6a5
VSevagen <43094266+VSevagen@users.noreply.github.com>
2018-10-26 22:07:20 -0700

    Update scaping.py

--- scaping.py
+++ scaping.py
@@ -2,8 +2,8 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-search_item = 'foss@amrita'
-url = 'https://www.google.co.in/search?q=' + search_item
+search_item = "foss@amrita"
+url = "https://www.google.co.in/search?q=" + search_item
 r = requests.get(url)
 html_content = r.text
 soup = BeautifulSoup(html_content, 'lxml')
